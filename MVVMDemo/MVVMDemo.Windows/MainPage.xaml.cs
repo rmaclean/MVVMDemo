9a169f47a2223b556e2245b3762d75c018cf2a8a
Robert MacLean <robert@sadev.co.za>
2015-04-15 10:44:09 +0200

    Part 1 demo stuff

--- MVVMDemo/MVVMDemo.Windows/MainPage.xaml.cs
+++ MVVMDemo/MVVMDemo.Windows/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using MVVMDemo.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,7 @@ namespace MVVMDemo
         public MainPage()
         {
             this.InitializeComponent();
+            this.DataContext = new MainPageViewModel();
         }
     }
 }
